4746a1bcef0f58cf7e094c1a42e2e7c0c2118a4d
Fati <reihaneh77@yahoo.com>
2017-09-21 19:45:18 +0200

    error1

--- TME1/TestMatriceEntiere.java
+++ TME1/TestMatriceEntiere.java
@@ -1,19 +1,22 @@
 /* Fatemeh Hamissi et Laura Nguyen*/
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+import java.io.Console;
 
 
 public class TestMatriceEntiere{
 	public static void main(String[] args){
-		String fichier = (System.console()).readline();
+		String fichier = (System.console()).readLine();
 		
 		try{
 			File f = new File(fichier);
 			MatriceEntiere m = new MatriceEntiere(f);
-			f.close();
+			f.closeFile();
 			System.out.println(m.toString());
 		}catch(FileNotFoundException e){
 			System.out.println("File not found");
